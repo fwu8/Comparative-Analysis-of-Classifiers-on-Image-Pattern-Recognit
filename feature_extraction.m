de6225623d8a90706aa5a6bf35b93c290b6b9298
WuFan <fwu8@ncsu.edu>
2015-09-28 07:50:33 +0800

    Add moment extraction part

--- feature_extraction.m
+++ feature_extraction.m
@@ -1,3 +1,16 @@
 % feature extraction
 load digittrain_dataset;
 % load xTrainImages(image) and tTrain(label)
+
+%imshow(xTrainImages{1});
+img = xTrainImages{2};
+mx = round(moment(img, 1, 0) / moment(img, 0, 0));
+my = round(moment(img, 0, 1) / moment(img, 0, 0));
+
+
+figure;
+imshow(img);
+
+img(mx, my) = 1;
+figure;
+imshow(img);
