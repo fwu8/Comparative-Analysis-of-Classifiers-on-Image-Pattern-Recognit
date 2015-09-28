ca454fb11bbac962e961b3199f277b5797735cdb
WuFan <fwu8@ncsu.edu>
2015-09-28 08:18:28 +0800

    Add test set

--- feature_extraction.m
+++ feature_extraction.m
@@ -3,5 +3,7 @@ load digittrain_dataset;
 % load xTrainImages(image) and tTrain(label)
 
 %imshow(xTrainImages{1});
-img = xTrainImages{2};
+img = xTrainImages{3};
 hu_moments(img)
+[xTestImages, tTest] = digittest_dataset;
+save('digittest_dataset', 'xTestImages', 'tTest');
