108e732d1e1e608717ff5b5f6ad2a29afbd90fe9
WuFan <fwu8@ncsu.edu>
2015-09-30 09:05:57 +0800

    Add HOG(Histogram Of Oriented gradients) feature, modify hu moments features

--- feature_extraction.m
+++ feature_extraction.m
@@ -1,9 +1,49 @@
 % feature extraction
-load digittrain_dataset;
+clc;
+clear;
 % load xTrainImages(image) and tTrain(label)
+load('digittrain_dataset.mat');
+load('digittest_dataset.mat');
 
-%imshow(xTrainImages{1});
-img = xTrainImages{3};
-hu_moments(img)
-[xTestImages, tTest] = digittest_dataset;
-save('digittest_dataset', 'xTestImages', 'tTest');
+%% feature extraction
+pos_num = 6;
+neg_num = 2;
+% Training data features
+pos_indx = find(tTrain(pos_num,:) == 1);
+neg_indx = find(tTrain(neg_num,:) == 1);
+train_features = [];
+for i = 1:length(pos_indx)
+    train_features(:, i) = ...
+        extractHOGFeatures(xTrainImages{pos_indx(i)},'CellSize',[10 10]);
+end
+for i = 1:length(neg_indx)
+    train_features(:, i+length(pos_indx)) = ...
+        extractHOGFeatures(xTrainImages{neg_indx(i)},'CellSize',[10 10]);
+end
+train_label = [zeros(1, length(pos_indx)), ones(1, length(neg_indx))];
+% Testing data features
+pos_indx = find(tTest(pos_num,:) == 1);
+neg_indx = find(tTest(neg_num,:) == 1);
+test_features = [];
+for i = 1:length(pos_indx)
+    test_features(:, i) = ...
+        extractHOGFeatures(xTestImages{pos_indx(i)},'CellSize',[10 10]);
+end
+for i = 1:length(neg_indx)
+    test_features(:, i+length(pos_indx)) = ...
+        extractHOGFeatures(xTestImages{neg_indx(i)},'CellSize',[10 10]);
+end
+test_label = [zeros(1, length(pos_indx)), ones(1, length(neg_indx))];
+
+%% Training
+net = newff(train_features,train_label, [20,1],{'tansig', 'purelin'});
+net.trainparam.epochs = 1000;
+net.trainparam.goal = 10^-5;
+net.trainParam.lr = 10^-1;
+net.trainParam.min_grad=1e-10;
+net = train(net,train_features,train_label);
+
+%% Testing
+predict = sim(net,test_features);
+predict = round(predict);
+acc = length(find(predict == test_label))/length(predict);
--- hu_moments.m
+++ hu_moments.m
@@ -1,21 +1,24 @@
-function [ m ] = hu_moments( img )
+function [ h ] = hu_moments( img )
 % This function calculate Hu moments of an input image.
+u20 = normalized_central_moment(img,2,0);
+u02 = normalized_central_moment(img,0,2);
+u11 = normalized_central_moment(img,1,1);
+u30 = normalized_central_moment(img,3,0);
+u12 = normalized_central_moment(img,1,2);
+u03 = normalized_central_moment(img,0,3);
+u21 = normalized_central_moment(img,2,1);
 
-m1 = normalized_central_moment(img,2,0) + ...
-    normalized_central_moment(img,0,2);
+h1 =  u20 + u02;
+h2 = h1^2 + 4*u11^2;
+h3 = (u30 - 3*u12)^2 + (u03 - 3*u21)^2;
+h4 = (u30 - u12)^2 + (u03 - u21)^2;
+h5 = (u30 - 3*u12)*(u30 + u12)*((u30 + u12)^2 - 3*(u21 + u03)^2) + ...
+    (3*u21 - u03)*(u21 + u03)*(3*(u30 + u12)^2 - (u21 + u03)^2);
+h6 = (u20 - u02)*((u30 + u12)^2 - (u21 + u03)^2) + ...
+    4*u11*(u30 + u12)*(u21 + u03);
+h7 = (3*u21 - u03)*(u21 + u03)*(3*(u30 + u12)^2 - (u21 + u03)^2) + ...
+    (3*u12 - u30)*(u12 + u30)*(3*(u30 + u12)^2 - (u21 + u03)^2);
 
-m2 = m1^2 + 4 * normalized_central_moment(img,1,1)^2;
-
-m3 = (normalized_central_moment(img,3,0) - ...
-    3*normalized_central_moment(img,1,2))^2 + ...
-    (normalized_central_moment(img,0,3) - ...
-    3*normalized_central_moment(img,2,1))^2;
-
-m4 = (normalized_central_moment(img,3,0) - ...
-    normalized_central_moment(img,1,2))^2 + ...
-    (normalized_central_moment(img,0,3) - ...
-    normalized_central_moment(img,2,1))^2;
-
-m = [m1, m2, m3, m4];
+h = [h1, h2, h3, h4, h5, h6, h7];
 end
 
